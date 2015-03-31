a61e5d1effacf9ea1260748b9e8f166f43b5845b
unknown <chris.c.mosey@gmail.com>
2015-03-31 18:08:34 -0400

    merged conflict with my self now fixed

--- js/createDb.js
+++ js/createDb.js
@@ -16,30 +16,24 @@ db.transaction(function(tx) {
 		+ "target_grade INTEGER(100)"
 		+ "credits INTEGER(12))", null, success, fail);
 
-<<<<<<< HEAD
 	tx.executeSql("CREATE TABLE IF NOT EXISTS tblTasks(")
 		+ "task_id INTEGER PRIMARY KEY,"
 		+ "class_id INTEGER(10),"
 		+ "task_type"
 		+ "task_name VARCHAR(30),"
 		+ "task_description VARCHAR(200),"
-=======
+
 	tx.executeSql("CREATE TABLE IF NOT EXISTS tblAssignments(")
 		+ "assignment_id INTEGER PRIMARY KEY,"
 		+ "class_id INTEGER(10),"
 		+ "assignment_type"
 		+ "assignment_name VARCHAR(30),"
 		+ "assignment_description VARCHAR(200),"
->>>>>>> 38ee9e17ed7b04586d79fd8b03cf51a4a44e376b
 		+ "target_grade INTEGER(100),"
 		+ "weight INTEGER(3),"
 		+ "weight_achieved INTEGER(3),"
 		+ "is_bonus BOOLEAN,"
-<<<<<<< HEAD
 		+ "FOREIGN KEY(class_id) REFERENCES tblClass(class_id)", null, success, fail);
-=======
-		+ "FOREIGN KEY(class_id) REFeRENCES tblClass(class_id)", null, success, fail);
->>>>>>> 38ee9e17ed7b04586d79fd8b03cf51a4a44e376b
 });
 
 function success(tx, result)
