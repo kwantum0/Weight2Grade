809e78d40b502e7541f2d521ec2baf9aad35c38a
unknown <dfranz-cc@2A411-C04.conestogac.on.ca>
2015-04-01 08:52:17 -0400

    it should of added a folder?

---
+++ js/global.js
@@ -0,0 +1,6 @@
+/*	global.js
+	runs on the initial set-up and binds event handlers
+
+	Revision history
+		Daniel Franz	 2015.04.01: created
+*/
---
+++ js/util.js
@@ -0,0 +1,6 @@
+/*	util.js
+	functions and methods to make the application work
+
+	Revision history
+		Daniel Franz	 2015.04.01: created
+*/
